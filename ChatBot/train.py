2867d1cbf62a0f3b64a5dc7e8b16137a0d5a6c2d
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2019-06-19 01:09:20 +0530

    bot code add

---
+++ ChatBot/train.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+import os
+import tensorflow_datasets as tfds
+import tensorflow as tf
+from bot import transformer
+from dataClean import VOCAB_SIZE, MAX_LENGTH, dataset, preprocess_sentence, START_TOKEN, tokenizer, END_TOKEN
+
+
+tf.keras.backend.clear_session()
+
+# Hyper-parameters
+NUM_LAYERS = 2
+D_MODEL = 256
+NUM_HEADS = 8
+UNITS = 512
+DROPOUT = 0.1
+
+model = transformer(
+    vocab_size=VOCAB_SIZE,
+    num_layers=NUM_LAYERS,
+    units=UNITS,
+    d_model=D_MODEL,
+    num_heads=NUM_HEADS,
+    dropout=DROPOUT)
+
+
+def loss_function(y_true, y_pred):
+    y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')(y_true, y_pred)
+
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+    loss = tf.multiply(loss, mask)
+
+    return tf.reduce_mean(loss)
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+    def __init__(self, d_model, warmup_steps=4000):
+        super(CustomSchedule, self).__init__()
+
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps**-1.5)
+
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+learning_rate = CustomSchedule(D_MODEL)
+
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+
+def accuracy(y_true, y_pred):
+    # ensure labels have shape (batch_size, MAX_LENGTH - 1)
+    y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+    accuracy = tf.metrics.SparseCategoricalAccuracy()(y_true, y_pred)
+    return accuracy
+
+
+model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
+
+EPOCHS = 20
+
+model.fit(dataset, epochs=EPOCHS)
+
+
+def evaluate(sentence):
+    sentence = preprocess_sentence(sentence)
+
+    sentence = tf.expand_dims(
+        START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
+    output = tf.expand_dims(START_TOKEN, 0)
+
+    for i in range(MAX_LENGTH):
+        predictions = model(inputs=[sentence, output], training=False)
+
+        # select the last word from the seq_len dimension
+        predictions = predictions[:, -1:, :]
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+        # return the result if the predicted_id is equal to the end token
+        if tf.equal(predicted_id, END_TOKEN[0]):
+            break
+
+        # concatenated the predicted_id to the output which is given to the decoder
+        # as its input.
+        output = tf.concat([output, predicted_id], axis=-1)
+
+    return tf.squeeze(output, axis=0)
+
+
+def predict(sentence):
+    prediction = evaluate(sentence)
+
+    predicted_sentence = tokenizer.decode(
+        [i for i in prediction if i < tokenizer.vocab_size])
+
+    print('Input: {}'.format(sentence))
+    print('Output: {}'.format(predicted_sentence))
+
+    return predicted_sentence
+
+
+sentence = 'I am not crazy, my mother had me tested.'
+for _ in range(5):
+    sentence = predict(sentence)
+    print('')
